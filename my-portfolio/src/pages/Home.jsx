35d2e73fc2fd6841e377e56a43dd548352cf1b91
thomasb40466 <thomasb40466@gmail.com>
2025-04-23 20:13:10 -0500

    Added base react

---
+++ my-portfolio/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Welcome to My Portfolio!</h1>
+      <p>This is the main page where I will share general information about myself.</p>
+    </div>
+  );
+};
+
+export default Home;
