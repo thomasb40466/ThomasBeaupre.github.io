eb7efcf6c6e05df6ec9f1bea7dc9b89180b8132a
thomasb40466 <142060338+thomasb40466@users.noreply.github.com>
2025-04-23 20:32:07 -0500

    a

--- my-portfolio/src/index.js
+++ my-portfolio/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css'; // You'll style here or with SCSS later
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<HashRouter>
-  <App />
-</HashRouter>
-
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
